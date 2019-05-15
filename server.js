e3f6286bbb8097b5b5988dcc677b663e093fffba
Jonathan Daly <dalyj138@gmail.com>
2019-05-14 20:20:07 -0400

    Added validateUserId and validateUser middleware. Also created GET request in userRouter.js.

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const USER_ROUTER = require('./users/userRouter');
 
 const server = express();
 server.use(express.json());
+
+server.use('/api/users', USER_ROUTER);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,44 +1,67 @@
 const express = require('express');
-const USERDB = require('./userDb');
+const USER_DB = require('./userDb');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/', validateUser, (req, res) => {
 
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', validateUserId, (req, res) => {
 
 });
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+    try {
+        const users = await USER_DB.get();
+        res.status(200).json(users);
+    } catch(err) {
+        res.status(500).json({ message: 'Error retrieving users!' });
+    }
+});
+
+router.get('/:id', validateUserId, (req, res) => {
 
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id/posts', validateUserId, (req, res) => {
 
 });
 
-router.get('/:id/posts', (req, res) => {
+router.delete('/:id', validateUserId, (req, res) => {
 
 });
 
-router.delete('/:id', (req, res) => {
-
-});
-
-router.put('/:id', (req, res) => {
+router.put('/:id', validateUserId, (req, res) => {
 
 });
 
 //custom middleware
 
-function validateUserId(req, res, next) {
-
+async function validateUserId(req, res, next) {
+    try{
+        const { id } = req.params;
+        const userId = await USER_DB.getById(id);
+        if(userId) {
+            req.user = userId;
+            next();
+        } else {
+            res.status(400).json({ message: 'invalid user id' });
+            next();
+        }
+    } catch(err) {
+        res.status(500).json({ message: 'Error validating user.' });
+    }
 };
 
 function validateUser(req, res, next) {
-
+    if(!req.body) {
+        res.status(400).json({ message: 'missing user data' });
+        next();
+    } else if(!req.body.name) {
+        res.status(400).json({ message: 'missing required name field' });
+        next();
+    }
 };
 
 function validatePost(req, res, next) {
