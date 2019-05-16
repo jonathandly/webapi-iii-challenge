094a8415c93075efaa4abe09817ae44b0e26b932
Jonathan Daly <dalyj138@gmail.com>
2019-05-15 20:49:16 -0400

    Added POST request for /:id/post route in userRouter.js. Created validatePostId middleware and GET request in postRouter.js. Project MVP complete.

--- server.js
+++ server.js
@@ -10,12 +10,12 @@ server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
-//custom middleware
-server.use(logger);
-
 function logger(req, res, next) {
   let now = new Date();
   console.log(`Request Type->${req.method} Request URL->${req.url} Timestamp->${now}`);
 };
+
+//custom middleware
+server.use(logger);
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,20 +1,29 @@
 const express = require('express');
 const USER_DB = require('./userDb');
+const POST_DB = require('../posts/postDb');
 
 const router = express.Router();
 
 router.post('/', validateUser, async (req, res) => {
+    
     try {
-        const post = await USER_DB.insert(req.name);
-        res.status(201).json(post);
+        const user = await USER_DB.insert(req.body);
+        res.status(201).json(user);
     } catch(err) {
         console.log(err);
-        res.status(500).json({ message: 'Post was unsuccessful' });
+        res.status(500).json({ message: 'Error adding user' });
     }
 });
 
-router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
-
+router.post('/:id/post', validateUserId, validatePost, async (req, res) => {
+    const user = {...req.body, userId: req.params.id };
+    try {
+        const post = await POST_DB.insert(user);
+        res.status(201).json(post);
+    } catch(err) {
+        console.log(err);
+        res.status(500).json({ message: 'Error adding post' });
+    }
 });
 
 router.get('/', async (req, res) => {
@@ -100,12 +109,12 @@ async function validateUserId(req, res, next) {
 };
 
 function validateUser(req, res, next) {
-    if(!req.body) {
-        res.status(400).json({ message: 'missing user data' });
+   
+    const name = req.body;
+    if(req.body && Object.keys(req.body).length) {
         next();
-    } else if(!req.body.name) {
-        res.status(400).json({ message: 'missing required name field' });
-        next();
+    } else {
+        res.status(404).json({ message: 'missing required name field' });
     }
 };
 
@@ -120,3 +129,4 @@ function validatePost(req, res, next) {
 };
 
 module.exports = router;
+
